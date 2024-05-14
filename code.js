bc97c1942fd2b39d6d2696938d86659b209b4efe
SniperMCM <162675216+SniperMCM@users.noreply.github.com>
2024-05-15 11:49:47 +1200

    Update code.js

--- code.js
+++ code.js
@@ -13,3 +13,4 @@ function distributedConsoleLogs() {
 distributedConsoleLogs();
 
 // paste this in someone's console and watch their PC explode into ashes
+// premium version: https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwia_oC3qo6GAxVGSmwGHXmXBZkQFnoECAYQAQ&url=https%3A%2F%2Fwww.roblox.com%2Fgame-pass%2F2611360%2Fbuy-for-500-robux&usg=AOvVaw1TbPzsxOvIj4hnVuKdsOad&opi=89978449
